84a00a9cd4df1a7b80a828e2fcd8bd7b3d613b5a
gnomesenpai <toshua_4_eve@hotmail.co.uk>
2019-05-22 01:30:26 +0100

    initial

---
+++ includes/stoptf2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#stop TF2 servers
+/home/game/mvmx10_1 stop
+/home/game/mvmx10_2 stop
+/home/game/mvmx10_3 stop
+/home/game/mvmx10_4 stop
+/home/game/mvmx10_5 stop
+/home/game/mvmx10_6 stop
+/home/game/mvmx10_7_machine_attacks stop
+/home/game/mvmx10_8 stop
+/home/game/randomizerx10_1 stop
+/home/game/cactuscanyon_1 stop
+/home/game/asteroid_1 stop
+/home/game/stockrotation_1 stop
+/home/game/dodgeball_1 stop
+/home/game/orange stop
+/home/game/mvmx20_1 stop
+/home/game/mvm_ma_2 stop
+
+
+
+#echo $(date '+%d %b %Y %H:%M:%S')."- Complete";
+#echo "-----"
---
+++ includes/updatetf2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+#tf2_machine_attacks directory
+/home/game/mvmx10_7_machine_attacks update
+#serverfiles directory
+/home/game/mvmx10_1 update
+#tf2_vanilla directory
+/home/game/mvmx10_3 update
+#tf2_nox10 directory
+/home/game/cactuscanyon_1 update
+
+
+#echo $(date '+%d %b %Y %H:%M:%S')."- Complete";
+#echo "-----"
