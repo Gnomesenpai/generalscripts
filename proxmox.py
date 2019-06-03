2c08843ea568682677cd1582312c664156816c98
gnomesenpai <toshua_4_eve@hotmail.co.uk>
2019-06-03 21:36:28 +0100

    work

--- proxmox.py
+++ proxmox.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+#!/usr/bin/env python3
+#testing information using psutil
+#install psutil with "pip3 install psutil"
+import psutil
 
 #lets define some things
 host="<proxmoxurl>"
@@ -8,11 +12,23 @@ port="<port>"
 user="<username>"
 password="<password>"
 
-answer = str(input("Would you like to create or delete an LXC? Enter Create or delete:\n"))
+answer = str(input("Would you like to create or delete an LXC? Enter Create or delete:\n>"))
 print('\n')
 if answer in ['create']:
     print("this is the create script")
+    answer = str(input("Pick an ID:\n>"))
+    #need to get the ID entered by the user
+    
+
 
 elif answer in ['delete']:
     print("ruh roh, you're getting deleted kiddo")
-    
+
+elif answer in ['info']:
+    print("Lets get some information!\n")
+    print('CPU Usage:', psutil.cpu_percent()),     
+    #print(psutil.virtual_memory())  # physical memory usage
+    print('memory used in %:', psutil.virtual_memory()[2])
+
+else:
+    print("No option selected!")
