dadce1cefdb7fac883ea28e368668ece9127559b
gnomesenpai <toshua_4_eve@hotmail.co.uk>
2019-06-15 22:27:50 +0100

    initial

---
+++ tf2servers_v2.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+version="Version 0.4"
+
+
+#lets define some things
+home="/home/game"
+includes="/includes"
+#LA details
+user1="game"
+host1="la.moevsmachine.tf"
+#other server details
+user2="game"
+host2="uk.moevsmachine.tf"
+
+#start servers
+if [ "$1" == "start"]; then
+    if [ "$2" == "uk"]
+
+    elif [ "$2" == "la"]
+
+    elif [ "$2" == "all"]
+
+    else
+    
+    fi
+
+#stop servers
+elif [ "$1" == "stop"]; then
+    if [ "$2" == "uk"]
+
+    elif [ "$2" == "la"]
+
+    elif [ "$2" == "all"]
+
+    else
+
+    fi
+
+#update servers
+elif [ "$1" == "update"]; then
+    if [ "$2" == "uk"]
+
+    elif [ "$2" == "la"]
+
+    elif [ "$2" == "all"]
+
+    else
+
+    fi
+
+#restart servers
+elif [ "$1" == "restart"]; then
+    if [ "$2" == "uk"]
+
+    elif [ "$2" == "la"]
+
+    elif [ "$2" == "all"]
+
+    else
+    
+    fi
+
+#pull system information
+elif [ "$1" == "info"]; then
+    if [ "$2" == "uk"]
+
+    elif [ "$2" == "la"]
+
+    else
+
+    fi
+
+else
+
+
+
+
+
