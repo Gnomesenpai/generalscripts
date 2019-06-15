625aaf8a7241469f1a10ebb207d7fa5445edb151
gnomesenpai <toshua_4_eve@hotmail.co.uk>
2019-06-15 22:34:01 +0100

    additions

--- tf2servers_v2.sh
+++ tf2servers_v2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version="Version 0.4"
+version="Version 0.1"
 
 
 #lets define some things
@@ -15,10 +15,13 @@ host2="uk.moevsmachine.tf"
 #start servers
 if [ "$1" == "start"]; then
     if [ "$2" == "uk"]
+        printf "Starting UK servers."
 
     elif [ "$2" == "la"]
+        printf "Starting LA servers."
 
     elif [ "$2" == "all"]
+        printf "Starting ALL servers."
 
     else
     
@@ -27,10 +30,13 @@ if [ "$1" == "start"]; then
 #stop servers
 elif [ "$1" == "stop"]; then
     if [ "$2" == "uk"]
+        printf "Stopping UK servers."
 
     elif [ "$2" == "la"]
+        printf "Stopping LA servers."
 
     elif [ "$2" == "all"]
+        printf "Stopping ALL servers."
 
     else
 
@@ -39,10 +45,13 @@ elif [ "$1" == "stop"]; then
 #update servers
 elif [ "$1" == "update"]; then
     if [ "$2" == "uk"]
+        printf "Updating UK servers."
 
     elif [ "$2" == "la"]
+        printf "Updating LA servers."
 
     elif [ "$2" == "all"]
+        printf "Updating ALL servers."
 
     else
 
@@ -51,10 +60,13 @@ elif [ "$1" == "update"]; then
 #restart servers
 elif [ "$1" == "restart"]; then
     if [ "$2" == "uk"]
+        printf "Restarting UK servers."
 
     elif [ "$2" == "la"]
+        printf "Restarting LA servers."
 
     elif [ "$2" == "all"]
+        printf "Restarting ALL servers."
 
     else
     
@@ -63,8 +75,10 @@ elif [ "$1" == "restart"]; then
 #pull system information
 elif [ "$1" == "info"]; then
     if [ "$2" == "uk"]
+        printf "Gathering UK information."
 
     elif [ "$2" == "la"]
+        printf "Gathering LA information."
 
     else
 
