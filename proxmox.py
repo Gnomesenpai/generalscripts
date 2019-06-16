6b77ec0bc876ac977a73e0f69abaf39ae1e7b09e
gnomesenpai <toshua_4_eve@hotmail.co.uk>
2019-06-16 01:42:43 +0100

    work

--- proxmox.py
+++ proxmox.py
@@ -2,17 +2,31 @@
 #!/usr/bin/env python3
 #testing information using psutil
 #install psutil with "pip3 install psutil"
-import psutil
+#import psutil
+import os
 
 #lets define some things
-host="<proxmoxurl>"
-port="<port>"
+port= <port>
+user= "<user>"
+host = "<host>"
+
 
 #maybe use SSH keys instead? for testing purposes user&pass will do
-user="<username>"
-password="<password>"
+#user = "<user>"
+#password="<password>"
 
-lxcstart = str(input("Would you like to create or delete an LXC? Enter Create or delete:\n>"))
+#def lxcstart(message):
+#    while True:
+#        try:
+#            lxcstart = int(input("Would you like to create or delete an LXC? Enter Create or delete:\n>"))
+#        except ValueError:
+#            print("Plese select a valid option.")
+#            continue
+#        else:
+#            return lxcstart
+#            break
+#id = lxcstart("Would you like to create or delete an LXC? Enter Create or delete:\n>")
+lxcstart = str(input("Would you like to create or delete an LXC? Enter Create or delete or type info for information:\n>"))
 print('\n')
 if lxcstart in ['create']:
     print("this is the create script")
@@ -49,7 +63,7 @@ if lxcstart in ['create']:
                 return vcore
                 break
     id = vcore("Select vCores: ")
-    print(type(vcore))
+    #print(type(vcore))
 
     def ram(message):
         while True:
@@ -62,8 +76,9 @@ if lxcstart in ['create']:
                 return ram
                 break
     id = ram("Select RAM in GB: ")
-    print(ram)
-    print=(vmid)
+    #print(ram)
+    #ram2 = "%d" % (ram)
+    #print(ram2)
     #example final string: pct create 999 local:vztmpl/debian-8.0-standard_8.0-1_amd64.tar.gz
     #example network string 2:pct set 999 -net0 name=eth0,bridge=vmbr0,ip=<ip address>/cidr,gw=<gateway>
 
@@ -71,10 +86,39 @@ elif lxcstart in ['delete']:
     print("ruh roh, you're getting deleted kiddo")
 
 elif lxcstart in ['info']:
-    print("Lets get some information!\n")
-    print('CPU Usage:', psutil.cpu_percent()),     
+    #get pve version
+    proxversion = "'pvesh get /version'"
+    proxversion2 = 'ssh -p %d %s@%s %s' % (port, user, host, proxversion)
+    #get uptime
+    proxuptime = "'uptime'"
+    proxuptime2 = 'ssh -p %d %s@%s %s' % (port, user, host, proxuptime)
+    #get storage information
+    proxstorage = "'df -h'"
+    proxstorage2 = 'ssh -p %d %s@%s %s' % (port, user, host, proxstorage)
+    print("Proxmox Information:\n")
+    os.system(proxversion2)
+    print("\n")
+    print("Server Uptime:\n")
+    os.system(proxuptime2)
+    print("\n")
+    print("Disk and mapping usage:\n")
+    os.system(proxstorage2)
+    print("\n")
+
+    
+    #print("Lets get some information!\n")
+    #print('CPU Usage:', psutil.cpu_percent()),     
     #print(psutil.virtual_memory())  # physical memory usage
-    print('memory used in %:', psutil.virtual_memory()[2])
+    #print('memory used in %:', psutil.virtual_memory()[2])
+ #   ssh root@$host
 
 else:
-    print("No option selected! Exiting!")
+    print("No option selected, exiting!")
+    #print(port)
+    #print(user)
+    #print(host)
+
+    #datainput = "'ls -la'"
+    #mycmd = 'ssh -p 8222 root@nagisa.gnome.moe "ls -la"'
+    #mycmd = 'ssh -p %d %s@%s %s' % (port, user, host, datainput)
+    #os.system(mycmd)
