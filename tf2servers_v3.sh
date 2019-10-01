5df5498d2a4f0ffb80c9ac55aae84874d8b946ca
gnomesenpai <toshua_4_eve@hotmail.co.uk>
2019-10-01 14:10:13 +0100

    push

--- tf2servers_v3.sh
+++ tf2servers_v3.sh
@@ -1,196 +1,88 @@
 #!/bin/bash
-version="Version 1.0"
+version="Version 1.1"
 
-#lets define some things
-home="/home/game"
-includes="/includes"
-#LA details
+#requires SSH keys
+#requres GNU parallel
+
+
+#server details
 user1="game"
-host1="la.moevsmachine.tf"
-#other server details
-user2="game"
-host2="uk.moevsmachine.tf"
+host1="uk.moevsmachine.tf"
 
 #start servers
 if [ "$1" == "start" ]; then
-    if [ "$2" == "uk" ]; then
-        echo "Starting UK servers."
-        ssh $user2@$host2 'parallel -k < "/home/game/includes/starttf2.sh"'
-        printf "Servers started!"
-        echo ""
-        echo $(date '+%d %b %Y %H:%M:%S')."- Complete";
-        echo "-----"
-
-    elif [ "$2" == "la" ]; then
-        echo "Starting LA servers."
-        ssh $user1@$host1 'parallel -k < "/home/game/includes/starttf2.sh"'
-        printf "Servers started!"
-        echo ""
-        echo $(date '+%d %b %Y %H:%M:%S')."- Complete";
-        echo "-----"
-
-    else
-        echo "Available Options:"
-        echo ""
-        echo -e "\tUK   -   Start UK Servers"
-        echo -e "\tLA   -   Start LA Servers"
-    fi
+   
+    echo "Starting UK servers."
+    ssh $user1@$host1 'parallel -k < "/home/game/includes/starttf2.sh"'
+    echo "Servers started!"
+    echo ""
+    echo $(date '+%d %b %Y %H:%M:%S')."- Complete";
+    echo "-----"
 
 #stop servers
 elif [ "$1" == "stop" ]; then
-    if [ "$2" == "uk" ]; then
-        echo "Stopping UK servers."
-        ssh $user2@$host2 'parallel -k < "/home/game/includes/stoptf2.sh"'
-        printf "Servers stopped!"
-        echo ""
-        echo $(date '+%d %b %Y %H:%M:%S')."- Complete";
-        echo "-----"
-
-    elif [ "$2" == "la" ]; then
-        echo "Stopping LA servers."
-        ssh $user1@$host1 'parallel -k < "/home/game/includes/stoptf2.sh"'
-        printf "Servers stopped!"
-        echo ""
-        echo $(date '+%d %b %Y %H:%M:%S')."- Complete";
-        echo "-----"
-
-    else
-        echo "Available Options:"
-        echo ""
-        echo -e "\tUK   -   Stop UK Servers"
-        echo -e "\tLA   -   Stop LA Servers"
-    fi
+    
+    echo "Stopping UK servers."
+    ssh $user1@$host1 'parallel -k < "/home/game/includes/stoptf2.sh"'
+    echo "Servers stopped!"
+    echo ""
+    echo $(date '+%d %b %Y %H:%M:%S')."- Complete";
+    echo "-----"
 
 #update servers
 elif [ "$1" == "update" ]; then
-    if [ "$2" == "uk" ]; then
-        echo "Inb4 localisation files, updating servers!"
-        printf "Stopping the servers"       #stop them
-        ssh $user2@$host2 'parallel -k < "/home/game/includes/stoptf2.sh"'
-        printf "Running update script"      #run the update script
-        #ssh $user2@$host2 'parallel -k < "/home/game/includes/updatetf2.sh"'
-        ssh $user2@$host2 '/home/game/mvmx10_7_machine_attacks update; /home/game/mvmx10_1 update; /home/game/mvmx10_3 update; /home/game/cactuscanyon_1 update'
-        printf  "Starting the servers"      #start them again
-        ssh $user2@$host2 'parallel -k < "/home/game/includes/starttf2.sh"'
-        echo ""
-        echo $(date '+%d %b %Y %H:%M:%S')."- Complete";
-        echo "-----"
 
-    elif [ "$2" == "la" ]; then
-        echo "Inb4 localisation files, updating servers!"
-        printf "Stopping the servers"       #stop them
-        ssh $user1@$host1 'parallel -k < "/home/game/includes/stoptf2.sh"'
-        printf "Running update script"      #run the update script
-        ssh $user1@$host1 'parallel -k < "/home/game/includes/updatetf2.sh"'
-        printf  "Starting the servers"      #start them again
-        ssh $user1@$host1 'parallel -k < "/home/game/includes/starttf2.sh"'
-        echo ""
-        echo $(date '+%d %b %Y %H:%M:%S')."- Complete";
-        echo "-----"
-    else
-        echo "Available Options:"
-        echo ""
-        echo -e "\tUK   -   Update UK Servers"
-        echo -e "\tLA   -   Update LA Servers"
-    fi
+    echo "Inb4 localisation files, updating servers!"
+    echo "Stopping the servers"       #stop them
+    ssh $user1@$host1 'parallel -k < "/home/game/includes/stoptf2.sh"'
+    echo "Running update script"      #run the update script
+    #ssh $user1@$host1 'parallel -k < "/home/game/includes/updatetf2.sh"'
+    ssh $user1@$host1 '/home/game/mvmx10_7_machine_attacks update; /home/game/mvmx10_1 update; /home/game/mvmx10_3 update; /home/game/cactuscanyon_1 update'
+    echo  "Starting the servers"      #start them again
+    ssh $user1@$host1 'parallel -k < "/home/game/includes/starttf2.sh"'
+    echo ""
+    echo $(date '+%d %b %Y %H:%M:%S')."- Complete";
+    echo "-----"
 
 #reboot servers
 elif [ "$1" == "restart" ]; then
-    if [ "$2" == "uk" ]; then
-        echo "Inb4 localisation files, updating servers!"
-        printf "Stopping the servers"       #stop them
-        ssh $user2@$host2 'parallel -k < "/home/game/includes/stoptf2.sh"'
-        printf  "Starting the servers"      #start them again
-        ssh $user2@$host2 'parallel -k < "/home/game/includes/starttf2.sh"'
-        echo ""
-        echo $(date '+%d %b %Y %H:%M:%S')."- Complete";
-        echo "-----"
 
-    elif [ "$2" == "la" ]; then
-        echo "Inb4 localisation files, updating servers!"
-        printf "Stopping the servers"       #stop them
-        ssh $user1@$host1 'parallel -k < "/home/game/includes/stoptf2.sh"'
-        printf  "Starting the servers"      #start them again
-        ssh $user1@$host1 'parallel -k < "/home/game/includes/starttf2.sh"'
-        echo ""
-        echo $(date '+%d %b %Y %H:%M:%S')."- Complete";
-        echo "-----"
-    else
-        echo "Available Options:"
-        echo ""
-        echo -e "\tUK   -   Restart UK Servers"
-        echo -e "\tLA   -   Restart LA Servers"
-    fi
+    echo "Inb4 localisation files, updating servers!"
+    echo "Stopping the servers"       #stop them
+    ssh $user1@$host1 'parallel -k < "/home/game/includes/stoptf2.sh"'
+    echo  "Starting the servers"      #start them again
+    ssh $user1@$host1 'parallel -k < "/home/game/includes/starttf2.sh"'
+    echo ""
+    echo $(date '+%d %b %Y %H:%M:%S')."- Complete";
+    echo "-----"
 
 #pull system information
 elif [ "$1" == "info" ]; then
-    if [ "$2" == "uk" ]; then
-        echo "Pulling UK stats"
-        echo ""
-        echo "Uptime"
-        ssh $user2@$host2 'uptime'
-        echo ""
-        echo "File System Usage"
-        ssh $user2@$host2 'df -h'
-        echo ""
-        echo "RAM Usage"
-        ssh $user2@$host2 'free -h'  
 
-    elif [ "$2" == "la" ]; then    
-        echo "Pulling LA stats"
-        echo ""
-        echo "Uptime"
-        ssh $user1@$host1 'uptime'
-        echo ""
-        echo "File System Usage"
-        ssh $user1@$host1 'df -h'
-        echo ""
-        echo "RAM Usage"
-        ssh $user1@$host1 'free -h'
-
-    elif [ "$2" == "all" ]; then    
-        echo "Pulling UK stats"
-        echo ""
-        echo "Uptime"
-        ssh $user2@$host2 'uptime'
-        echo ""
-        echo "File System Usage"
-        ssh $user2@$host2 'df -h'
-        echo ""
-        echo "RAM Usage"
-        ssh $user2@$host2 'free -h' 
-        echo "Pulling LA stats"
-        echo ""
-        echo "Uptime"
-        ssh $user1@$host1 'uptime'
-        echo ""
-        echo "File System Usage"
-        ssh $user1@$host1 'df -h'
-        echo ""
-        echo "RAM Usage"
-        ssh $user1@$host1 'free -h'  
-
-    else
-        echo "Available Options:"
-        echo ""
-        echo -e "\tUK     -   UK Resource Usages & Uptime"
-        echo -e "\tLA     -   LA Resource Usages & Uptime"
-        echo -e "\tall    -   All Resource Usages & Uptime"
-    fi
+    echo "Pulling UK stats"
+    echo ""
+    echo "Uptime"
+    ssh $user1@$host1 'uptime'
+    echo ""
+    echo "File System Usage"
+    ssh $user1@$host1 'df -h'
+    echo ""
+    echo "RAM Usage"
+    ssh $user1@$host1 'free -h'  
 
 #print options
 else
-echo ""
-echo "Available Options:"
-echo ""
-echo -e "\tStart     -   start TF2 servers"
-echo -e "\tStop      -   stop TF2 servers"
-echo -e "\tUpdate    -   update TF2 servers"
-echo -e "\tRestart   -   restart TF2 servers"
-echo -e "\tInfo      -   Pull Local & Remote Uptime, Disk usage, memory usage & load average"
-echo ""
-echo "$version"
-echo ""
+    echo ""
+    echo "Available Options:"
+    echo ""
+    echo -e "\tStart     -   start TF2 servers"
+    echo -e "\tStop      -   stop TF2 servers"
+    echo -e "\tUpdate    -   update TF2 servers"
+    echo -e "\tRestart   -   restart TF2 servers"
+    echo -e "\tInfo      -   Pull local disk usage, memory usage & load average"
+    echo ""
+    echo "$version"
+    echo ""
 fi
 #echo ""
 #echo $(date '+%d %b %Y %H:%M:%S')."- Complete";
