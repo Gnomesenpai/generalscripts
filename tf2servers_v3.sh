58e6f7e80d0afc05f245fd00e9b79cf73a7ab04d
gnomesenpai <toshua_4_eve@hotmail.co.uk>
2020-10-03 00:36:13 +0100

    added l4d2

--- tf2servers_v3.sh
+++ tf2servers_v3.sh
@@ -30,18 +30,36 @@ elif [ "$1" == "stop" ]; then
 
 #update servers
 elif [ "$1" == "update" ]; then
+    if [ "$2" == "tf2" ]; then
+        echo "Inb4 localisation files, updating servers!"
+        sleep 5s
+        echo "Stopping the servers"       #stop them
+        ssh $user1@$host1 'parallel -k < "/home/game/includes/stoptf2.sh"'
+        echo "Running update script"      #run the update script
+        #ssh $user1@$host1 'parallel -k < "/home/game/includes/updatetf2.sh"'
+        ssh $user1@$host1 '/home/game/mvmx10_7_machine_attacks update; /home/game/mvmx10_1 update; /home/game/mvmx10_3 update; /home/game/cactuscanyon_1 update'
+        echo  "Starting the servers"      #start them again
+        ssh $user1@$host1 'parallel -k < "/home/game/includes/starttf2.sh"'
+        echo ""
+        echo $(date '+%d %b %Y %H:%M:%S')."- Complete";
+        echo "-----"
+    elif [ "$2" == "l4d2" ]; then
+        echo "updating L4D2"
+        sleep 5s
+        echo "Stopping the servers"       #stop them
+        #ssh $user1@$host1 'parallel -k < "/home/game/includes/stoptf2.sh"'
+        echo "Running update script, this will freeze on waiting for user info..."      #run the update script
+        #ssh $user1@$host1 '/home/game/includes/update_l4d2.sh'
+        ssh $user1@$host1 '/home/game/steamcmd/steamcmd.sh +login "anonymous"  +force_install_dir "/home/game/l4d2_srv" +app_update "222860"  validate +quit'
+        echo  "Starting the servers"      #start them again
+        ssh $user1@$host1 '/home/game/l4d2_1.sh; /home/game/l4d2_2.sh; /home/game/l4d2_3.sh; /home/game/l4d2_4.sh;'
+        echo ""
+        echo $(date '+%d %b %Y %H:%M:%S')."- Complete";
+        echo "-----"
+    else
+        echo "no option selected"
+    fi
 
-    echo "Inb4 localisation files, updating servers!"
-    echo "Stopping the servers"       #stop them
-    ssh $user1@$host1 'parallel -k < "/home/game/includes/stoptf2.sh"'
-    echo "Running update script"      #run the update script
-    #ssh $user1@$host1 'parallel -k < "/home/game/includes/updatetf2.sh"'
-    ssh $user1@$host1 '/home/game/mvmx10_7_machine_attacks update; /home/game/mvmx10_1 update; /home/game/mvmx10_3 update; /home/game/cactuscanyon_1 update'
-    echo  "Starting the servers"      #start them again
-    ssh $user1@$host1 'parallel -k < "/home/game/includes/starttf2.sh"'
-    echo ""
-    echo $(date '+%d %b %Y %H:%M:%S')."- Complete";
-    echo "-----"
 
 #reboot servers
 elif [ "$1" == "restart" ]; then
@@ -84,11 +102,11 @@ elif [ "$1" == "startup" ]; then
     ssh $user1@$host1 'parallel -k < "/home/game/includes/starttf2.sh"'
     echo "Servers started!"
     echo ""
-    echo "Starting Insurgency servers"
-    ssh $user1@$host1 '/home/game/insserver start; /home/game/insserver_1 start'
-    echo ""
+    #echo "Starting Insurgency servers"
+    #ssh $user1@$host1 '/home/game/insserver start; /home/game/insserver_1 start'
+    #echo ""
     echo "Starting L4D2 servers"
-    ssh $user1@$host1 '/home/game/l4d2_1.sh; /home/game/l4d2_2.sh; /home/game/l4d2_3.sh;'
+    ssh $user1@$host1 '/home/game/l4d2_1.sh; /home/game/l4d2_2.sh; /home/game/l4d2_3.sh; /home/game/l4d2_4.sh;'
     echo $(date '+%d %b %Y %H:%M:%S')."- Complete";
     echo "-----"
 
@@ -161,6 +179,7 @@ else
     echo -e "\tShutdown  -   Stop all core servers/services"
     echo -e "\tMirror    -   Pull TF2C mirror information"
     echo -e "\tHost      -   Pull host information - BROKEN"
+    echo -e "\t"
     echo ""
     echo "$version"
     echo ""
