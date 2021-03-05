4f86938d45f41a24cf309cd94f344803e09bca7e
gnomesenpai <toshua_4_eve@hotmail.co.uk>
2021-03-05 22:05:19 +0000

    push

---
+++ apache.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+if [[ $(systemctl is-active apache2)=active ]]; then
+    echo "Apache is active"
+else
+    echo "Apache is ded fam"
+fi
--- tf2servers_v3.sh
+++ tf2servers_v3.sh
@@ -10,23 +10,41 @@ host3="miku.gnome.moe"
 #start servers
 #fixes formatting/line ending issues between platforms.
 dos2unix "tf2servers_v3.sh"
+echo ""
 #start main funtions
 if [ "$1" == "start" ]; then
-    echo "Starting UK servers."
-    ssh $user1@$host1 'parallel -k < "/home/game/includes/starttf2.sh"'
-    echo "Servers started!"
-    echo ""
-    echo $(date '+%d %b %Y %H:%M:%S')."- Complete";
-    echo "-----"
-
+    if [ "$2" == "tf2" ]; then
+        echo "Starting UK servers."
+        ssh $user1@$host1 'parallel -k < "/home/game/includes/starttf2.sh"'
+        echo "Servers started!"
+        echo ""
+        echo $(date '+%d %b %Y %H:%M:%S')."- Complete";
+        echo "-----"
+    elif [ "$2" == "l4d2" ]; then
+        echo "Stopping the servers"       #stop them
+        ssh $user1@$host1 '/home/game/l4d2_1.sh; /home/game/l4d2_2.sh; /home/game/l4d2_3.sh; /home/game/l4d2_4.sh;'
+    else
+        echo "No option selected"
+        echo -e "\tstart tf2      -   Start TF2 servers"
+        echo -e "\tstart l4d2     -   Start L4D2 servers"
+    fi
 #stop servers
 elif [ "$1" == "stop" ]; then
-    echo "Stopping UK servers."
-    ssh $user1@$host1 'parallel -k < "/home/game/includes/stoptf2.sh"'
-    echo "Servers stopped!"
-    echo ""
-    echo $(date '+%d %b %Y %H:%M:%S')."- Complete";
-    echo "-----"
+    if [ "$2" == "tf2" ]; then
+        echo "Stopping UK servers."
+        ssh $user1@$host1 'parallel -k < "/home/game/includes/stoptf2.sh"'
+        echo "Servers stopped!"
+        echo ""
+        echo $(date '+%d %b %Y %H:%M:%S')."- Complete";
+        echo "-----"
+    elif [ "$2" == "l4d2" ]; then
+        echo "Stopping the servers"       #stop them
+        ssh $user1@$host1 'screen -S l4d2_1 -X quit; screen -S l4d2_2 -X quit; screen -S l4d2_3 -X quit; screen -S l4d2_4 -X quit'
+    else
+        echo "No option selected"
+        echo -e "\tstop tf2      -   Stop TF2 servers"
+        echo -e "\tstop l4d2     -   Stop L4D2 servers"
+    fi
 
 #update servers
 elif [ "$1" == "update" ]; then
@@ -36,7 +54,6 @@ elif [ "$1" == "update" ]; then
         echo "Stopping the servers"       #stop them
         ssh $user1@$host1 'parallel -k < "/home/game/includes/stoptf2.sh"'
         echo "Running update script"      #run the update script
-        #ssh $user1@$host1 'parallel -k < "/home/game/includes/updatetf2.sh"'
         ssh $user1@$host1 '/home/game/mvmx10_7_machine_attacks update; /home/game/mvmx10_1 update; /home/game/mvmx10_3 update; /home/game/cactuscanyon_1 update'
         echo  "Starting the servers"      #start them again
         ssh $user1@$host1 'parallel -k < "/home/game/includes/starttf2.sh"'
@@ -47,31 +64,45 @@ elif [ "$1" == "update" ]; then
         echo "updating L4D2"
         sleep 5s
         echo "Stopping the servers"       #stop them
-        #ssh $user1@$host1 'parallel -k < "/home/game/includes/stoptf2.sh"'
+        ssh $user1@$host1 'screen -S l4d2_1 -X quit; screen -S l4d2_2 -X quit; screen -S l4d2_3 -X quit; screen -S l4d2_4 -X quit'
         echo "Running update script, this will freeze on waiting for user info..."      #run the update script
-        #ssh $user1@$host1 '/home/game/includes/update_l4d2.sh'
-        ssh $user1@$host1 '/home/game/steamcmd/steamcmd.sh +login "anonymous"  +force_install_dir "/home/game/l4d2_srv" +app_update "222860"  validate +quit'
+        ssh $user1@$host1 '/home/game/includes/update_l4d2.sh'
         echo  "Starting the servers"      #start them again
         ssh $user1@$host1 '/home/game/l4d2_1.sh; /home/game/l4d2_2.sh; /home/game/l4d2_3.sh; /home/game/l4d2_4.sh;'
+        echo "Checking status"
+        ssh $user1@$host1 'screen -list | grep "l4d2"'
         echo ""
         echo $(date '+%d %b %Y %H:%M:%S')."- Complete";
         echo "-----"
     else
         echo "no option selected"
+        echo -e "\tupdate tf2      -   Update TF2 servers"
+        echo -e "\tupdate l4d2     -   Update L4D2 servers"
     fi
 
 
 #reboot servers
 elif [ "$1" == "restart" ]; then
-
-    echo "Restarting all servers!"
-    echo "Stopping the servers"       #stop them
-    ssh $user1@$host1 'parallel -k < "/home/game/includes/stoptf2.sh"'
-    echo  "Starting the servers"      #start them again
-    ssh $user1@$host1 'parallel -k < "/home/game/includes/starttf2.sh"'
-    echo ""
-    echo $(date '+%d %b %Y %H:%M:%S')."- Complete";
-    echo "-----"
+    if [ "$2" == "tf2" ]; then
+        echo "Restarting TF2 servers!"
+        echo "Stopping the servers"       #stop them
+        ssh $user1@$host1 'parallel -k < "/home/game/includes/stoptf2.sh"'
+        echo  "Starting the servers"      #start them again
+        ssh $user1@$host1 'parallel -k < "/home/game/includes/starttf2.sh"'
+        echo ""
+        echo $(date '+%d %b %Y %H:%M:%S')."- Complete";
+        echo "-----"
+    elif [ "$2" == "l4d2" ]; then
+        echo "Restarting L4D2 severs!"
+        echo "Stopping the servers"       #stop them
+        ssh $user1@$host1 'screen -S l4d2_1 -X quit; screen -S l4d2_2 -X quit; screen -S l4d2_3 -X quit; screen -S l4d2_4 -X quit'
+        echo "Starting L4D2 servers"
+        ssh $user1@$host1 '/home/game/l4d2_1.sh; /home/game/l4d2_2.sh; /home/game/l4d2_3.sh; /home/game/l4d2_4.sh;'
+    else
+        echo "no option selected"
+        echo -e "\trestart tf2      -   Restart TF2 servers"
+        echo -e "\trestart l4d2     -   Restart L4D2 servers"
+    fi
 
 #pull system information
 elif [ "$1" == "info" ]; then
@@ -95,9 +126,9 @@ elif [ "$1" == "startup" ]; then
     echo "Starting Discord bot"
     echo ""
     ssh $user1@$host1 '/home/game/start_discord.sh'
-    #echo "Starting Minecraft Server"
+    echo "Starting Minecraft Server"
     echo ""
-    #ssh $user1@$host1 '/home/game/mc_perfectlycomplex.sh'
+    ssh $user1@$host1 'cd /home/game/minecraft6/ && ./startmc.sh'
     echo "Starting TF2 Servers."
     ssh $user1@$host1 'parallel -k < "/home/game/includes/starttf2.sh"'
     echo "Servers started!"
@@ -106,7 +137,8 @@ elif [ "$1" == "startup" ]; then
     #ssh $user1@$host1 '/home/game/insserver start; /home/game/insserver_1 start'
     #echo ""
     echo "Starting L4D2 servers"
-    ssh $user1@$host1 '/home/game/l4d2_1.sh; /home/game/l4d2_2.sh; /home/game/l4d2_3.sh; /home/game/l4d2_4.sh;'
+    ssh $user1@$host1 '/home/game/l4d2_1.sh && sleep 5s && /home/game/l4d2_2.sh && sleep 5s && /home/game/l4d2_3.sh && sleep 5s && /home/game/l4d2_4.sh;'
+    ssh $user1@$host1 'screen -list'
     echo $(date '+%d %b %Y %H:%M:%S')."- Complete";
     echo "-----"
 
@@ -114,18 +146,20 @@ elif [ "$1" == "startup" ]; then
 elif [ "$1" == "shutdown" ]; then
     echo "Stopping Discord bot"
     echo ""
-    echo "REQUIRES MANUAL SHUTDOWN"
-    #ssh $user1@$host1 '/home/game/start_discord.sh'
+    ssh $user1@$host1 'screen -S discordbot -X quit'
     echo "Stopping Minecraft Server"
     echo ""
+    #ssh $user1@$host1 'screen -S discordbot -X quit'
     echo "REQUIRES MANUAL SHUTDOWN"
-    #ssh $user1@$host1 '/home/game/mc_perfectlycomplex.sh'
     echo "Stopping TF2 Servers."
     ssh $user1@$host1 'parallel -k < "/home/game/includes/stoptf2.sh"'
     echo "Servers down!"
     echo ""
     echo "Stopping Insurgency servers"
-    ssh $user1@$host1 '/home/game/insserver stop; /home/game/insserver_1 stop'
+    #ssh $user1@$host1 '/home/game/insserver stop; /home/game/insserver_1 stop'
+    echo "Stopping the L4D2 servers"       #stop them
+    ssh $user1@$host1 'screen -S l4d2_1 -X quit; screen -S l4d2_2 -X quit; screen -S l4d2_3 -X quit; screen -S l4d2_4 -X quit'
+
 
     echo $(date '+%d %b %Y %H:%M:%S')."- Complete";
     echo "-----"
@@ -170,10 +204,10 @@ else
     echo ""
     echo "Available Options:"
     echo ""
-    echo -e "\tStart     -   Start TF2 servers"
-    echo -e "\tStop      -   Stop TF2 servers"
-    echo -e "\tUpdate    -   Update TF2 servers"
-    echo -e "\tRestart   -   Restart TF2 servers"
+    echo -e "\tStart     -   Start servers"
+    echo -e "\tStop      -   Stop servers"
+    echo -e "\tUpdate    -   Update servers"
+    echo -e "\tRestart   -   Restart servers"
     echo -e "\tInfo      -   Pull local disk usage, memory usage & load average"
     echo -e "\tStartup   -   Start all core servers/services"
     echo -e "\tShutdown  -   Stop all core servers/services"
