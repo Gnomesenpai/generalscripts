0a08c8e3fb4b6e551e01398813add6bab81e5b48
gnomesenpai <toshua_4_eve@hotmail.co.uk>
2022-04-15 23:23:22 +0100

    additions

--- lazydd.py
+++ lazydd.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 #!/usr/bin/env python3
 
+
+#TO TO:
+# re-write using hwinfo --disks --short for disk information
+# assume os is installed on /dev/sda and asume b-z is in nuking range
+#
 import os
 import string
-
+print("This script required hwinfo")
 print("THIS UTILITY CAN WIPE ALL DATA!!!!!!")
 confirmdelete1 = str(input("Are you sure you wish to continue? (yes/no): ")).lower()
 
@@ -12,9 +17,11 @@ confirmdelete2 = str(input("Are you REALLY sure you wish to continue? (yes/no): 
 
 if confirmdelete1 == 'yes' and confirmdelete2 == 'yes':
     print("responses match")
-    fdisk = 'fdisk --list | grep  "Disk /dev/" | grep -v  -i 'Partition 2''
-'
-    os.system(fdisk)
+    #fdisk = 'fdisk --list | grep  "Disk /dev/" | grep -v  -i 'Partition 2''
+    diskinfo = 'hwinfo --disk --short'
+
+    #os.system(fdisk)
+    os.system(diskinfo)
 else:
     print("Responses dont match")
     exit()
