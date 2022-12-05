ca80404e6d5d3febfba597777d963badd120b8ee
gnomesenpai <toshua_4_eve@hotmail.co.uk>
2022-12-05 21:04:23 +0000

    awyup

--- bulkcopy/serverhopcopy.sh
+++ bulkcopy/serverhopcopy.sh
@@ -18,7 +18,7 @@ stt="/${base}/stop_that_tank/${suffix}"
 parallel cp -vf "/${base2}/bulkcopy/serverhop.cfg" :::  $maattk $ao3 $joinblu $nox10 $uu $mvmx10 $stt
 
 #parallel rsync to LA
-parallel rsync --progress  -vhz "${base}/bulkcopy/serverhop.cfg" ::: game@aus.moevsmachine.tf:$mvmx10 game@aus.moevsmachine.tf:$joinblu
+parallel rsync --progress  -vhz "${base2}/bulkcopy/serverhop.cfg" ::: game@aus.moevsmachine.tf:$mvmx10 game@aus.moevsmachine.tf:$joinblu
 echo $(date '+%d %b %Y %H:%M:%S')."- Complete";
 echo "-----"
 
